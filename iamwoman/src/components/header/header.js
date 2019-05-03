2a38ad96506a360aa48b215b56e096c75088acf5
Serg Ve <testfrontol@gmail.com>
2019-05-04 00:54:17 +0300

    Beginning with Webpack 4 and Pug

---
+++ iamwoman/src/components/header/header.js
@@ -0,0 +1,4 @@
+
+import './header.css';
+import '../logo/logo.css';
+import '../nav/nav.css';
---
+++ iamwoman/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public')
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin({}),
+      new OptimizeCSSAssetsPlugin({})]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    port: 8081
+  },
+  plugins: [
+    new HTMLPlugin({
+      filename: "index.html",
+      template: "./src/index.pug"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+  module: {
+    rules: [
+
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
